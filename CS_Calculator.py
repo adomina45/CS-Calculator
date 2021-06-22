f041b9b57a89ddf3e0ec5968fd8bf9c43afb6a24
adomina45 <agdomi22@g.holycross.edu>
2021-06-22 02:57:59 -0400

    Update CS_Calculator.py
    Replaced alpha numeric keypad with textbox that allows for entry from keyboard, also have completed binary logarithm for decimal.

--- CS_Calculator.py
+++ CS_Calculator.py
@@ -5,29 +5,31 @@ import math
 
 sg.theme("Python")
 layout = [[sg.Text("How can I help you?")], 
- [sg.Output(size=(50,1), key='-OUTPUT-')], 
  [sg.Text("What number system are you using? (If nothing is input then we will default to decimal)")],
  [sg.Button("Binary"), sg.Button("Decimal"), sg.Button("Hexadecimal"), sg.Button("Octal")],
  [sg.Text("What's the number?")],
- [sg.Button("0"), sg.Button("1"), sg.Button("2"), sg.Button("3")],
- [sg.Button("4"), sg.Button("5"), sg.Button("6"), sg.Button("7")],
- [sg.Button("8"), sg.Button("9"), sg.Button("A"), sg.Button("B")],
- [sg.Button("C"), sg.Button("D"), sg.Button("E"), sg.Button("F")],
- [sg.Button(".")],
+ [sg.InputText(key='textbox')],
+ #[sg.Button("0"), sg.Button("1"), sg.Button("2"), sg.Button("3")],
+ #[sg.Button("4"), sg.Button("5"), sg.Button("6"), sg.Button("7")],
+ #[sg.Button("8"), sg.Button("9"), sg.Button("A"), sg.Button("B")],
+ #[sg.Button("C"), sg.Button("D"), sg.Button("E"), sg.Button("F")],
+ #[sg.Button(".")],
  [sg.Text("What are we doing to the number?")],
- [sg.Button("Binary Log"), sg.Button("Convert to Binary"), sg.Button("Convert to Decimal")], 
- [sg.Button("Convert to Hexadecimal"), sg.Button("Convert to Octal"), sg.Button("Quit")]]
-
+ [sg.Button("Binary Logarithm"), sg.Button("Convert to Binary"), sg.Button("Convert to Decimal")], 
+ [sg.Button("Convert to Hexadecimal"), sg.Button("Convert to Octal")], 
+ [sg.Output(size=(50,1), key='-OUTPUT-')],
+ [sg.Button("Quit")]]
+ 
 # Create the window
 window = sg.Window("Calcator", layout, margins=(100, 50))
 
-while True:
+continue_calculating = True
+while continue_calculating:
     event, values = window.read()
-    num_system = "decimal" #if not specificed will default to decimal
-    float = False
-    number = ""   
+    input = values['textbox'] #grab input from textbox
+    num_system = "decimal" #if not specificed will default to decimal  
     if event == sg.WIN_CLOSED or event == "Quit":
-        break
+        continue_calculating = False
     elif event == "Binary":
         num_system = "binary"
     elif event == "Hexadecimal":
@@ -36,36 +38,17 @@ while True:
         num_system = "octal"
     elif event == "Decimal":
         num_system = "decimal"
-    elif event == "." or event == "Binary Log" or event == "Convert to Binary" or event == "Convert to Decimal" or event == "Convert to Hexadecimal" or event == "Convert to Octal":
-        noop
-    else:
-        stop = False
-        number = event
-        print (number, end = '')
-        while stop != True:
-            new_event, new_values = window.read()
-            if new_event == sg.WIN_CLOSED or new_event == "Quit":
-                break
-            elif new_event == "Binary Log":
-                if float == True:
-                    f = float(number)
-                    final = math.log(f,2)
-                    print("\n")
-                    print(final)
-                else:
-                    i = int(number)
-                    final = math.log(i,2)
-                    print("\n")
-                    print(final)
-                stop = True
-            elif new_event == ".": 
-                if num_system == "decimal":
-                    print (new_event, end = '')
-                else:
-                    noop
-            else:
-                number = number + new_event
-                print(new_event, end = '')
+    elif event == "Binary Logarithm": 
+        try:
+            f = float(input)
+            final = math.log(f,2)
+            print(final)
+        except:
+            print("The data you have input is of an invalid type")
+    #elif event == "Convert to Binary": 
+    #elif event == "Convert to Decimal": 
+    #elif event == "Convert to Hexadecimal": 
+    #elif event == "Convert to Octal":
                 
     
         
