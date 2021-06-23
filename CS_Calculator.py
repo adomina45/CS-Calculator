ed15204b99613800f7f50ec7ab0e58dab7276bdc
ogfani <ogfani22@g.holycross.edu>
2021-06-22 22:31:29 -0400

    Update CS_Calculator.py
    There are errors within the buttons that I looked at first. The conversion between the floating point and decimals seems to work in theory, but we need to figure out the buttons before we can test it out. I am exhausted and will spend more time with it tomorrow

--- CS_Calculator.py
+++ CS_Calculator.py
@@ -4,7 +4,7 @@ import PySimpleGUI as sg
 import math
 
 sg.theme("Python")
-layout = [[sg.Text("How can I help you?")], 
+layout = [[sg.Text("How can I help you?")],
  [sg.Text("What number system are you using? (If nothing is input then we will default to decimal)")],
  [sg.Button("Binary"), sg.Button("Decimal"), sg.Button("Hexadecimal"), sg.Button("Octal")],
  [sg.Text("What's the number?")],
@@ -15,11 +15,11 @@ layout = [[sg.Text("How can I help you?")],
  #[sg.Button("C"), sg.Button("D"), sg.Button("E"), sg.Button("F")],
  #[sg.Button(".")],
  [sg.Text("What are we doing to the number?")],
- [sg.Button("Binary Logarithm"), sg.Button("Convert to Binary"), sg.Button("Convert to Decimal")], 
- [sg.Button("Convert to Hexadecimal"), sg.Button("Convert to Octal")], 
+ [sg.Button("Binary Logarithm"), sg.Button("Convert to Binary"), sg.Button("Convert to Decimal")],
+ [sg.Button("Convert to Hexadecimal"), sg.Button("Convert to Octal")],
  [sg.Output(size=(50,1), key='-OUTPUT-')],
  [sg.Button("Quit")]]
- 
+
 # Create the window
 window = sg.Window("Calcator", layout, margins=(100, 50))
 
@@ -27,7 +27,7 @@ continue_calculating = True
 while continue_calculating:
     event, values = window.read()
     input = values['textbox'] #grab input from textbox
-    num_system = "decimal" #if not specificed will default to decimal  
+    num_system = "decimal" #if not specificed will default to decimal
     if event == sg.WIN_CLOSED or event == "Quit":
         continue_calculating = False
     elif event == "Binary":
@@ -38,20 +38,22 @@ while continue_calculating:
         num_system = "octal"
     elif event == "Decimal":
         num_system = "decimal"
-    elif event == "Binary Logarithm": 
+    elif event == "Binary Logarithm":
         try:
             f = float(input)
             final = math.log(f,2)
             print(final)
         except:
             print("The data you have input is of an invalid type")
-    #elif event == "Convert to Binary": 
-    #elif event == "Convert to Decimal": 
-    #elif event == "Convert to Hexadecimal": 
+
+
+
+
+
+
+
+
+    #elif event == "Convert to Binary":
+    #elif event == "Convert to Decimal":
+    #elif event == "Convert to Hexadecimal":
     #elif event == "Convert to Octal":
-                
-    
-        
-    
- 
-           
