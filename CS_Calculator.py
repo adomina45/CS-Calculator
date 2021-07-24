dd36e2634b586dc780d2edde6380fa3f4d79c869
adomina45 <agdomi22@g.holycross.edu>
2021-07-23 21:20:40 -0400

    It is complete
    The executable can be used to run the file on windows.

--- CS_Calculator.py
+++ CS_Calculator.py
@@ -12,7 +12,7 @@ layout = [[sg.Text("How can I help you?")],
  [sg.Text("What are we doing to the number?")],
  [sg.Button("Binary Logarithm"), sg.Button("Convert to Binary"), sg.Button("Convert to Decimal")],
  [sg.Button("Convert to Hexadecimal"), sg.Button("Convert to Octal")],
- [sg.Output(size=(50,1), key='-OUTPUT-')],
+ [sg.Output(size=(50,5), key='-OUTPUT-')],
  [sg.Button("Quit")]]
 
 # Create the window
@@ -48,27 +48,92 @@ while continue_calculating:
                 print(final)
             except:
                 print("The data you have input is not a binary number")
+        elif num_system == "hexadecimal":
+            try:
+                i = int(input, 16)
+                final = math.log(i,2)
+                print(final)
+            except:
+                print("The data you have input is not a hexadecimal number")
+        elif num_system == "octal":
+            try:
+                i = int(input, 8)
+                final = math.log(i,2)
+                print(final)
+            except:
+                print("The data you have input is not a octal number")
     elif event == "Convert to Binary":
         if num_system == "decimal":
             try:
-                num = int(input)
-                b = bin(num)
+                i = int(input)
+                b = bin(i)
                 print(b)
             except:
                 print("The data you have input is not a decimal integer")
         elif num_system == "binary":
             print("You already have binary selectred. You need to change to another system in order to convert to decimal.")
+        elif num_system == "hexadecimal":
+            try:
+                i = int(input, 16)
+                b = bin(i)
+                print(b)
+            except:
+                print("The data you have input is not a hexadecimal integer")
+        elif num_system == "octal":
+            try:
+                i = int(input, 8)
+                b = bin(i)
+                print(b)
+            except:
+                print("The data you have input is not a octal integer")
     elif event == "Convert to Decimal":
         if num_system == "decimal":
             print("You already have decimal selected. You need to change to another number system in order to convert to decimal.")
+        elif num_system == "binary": 
+            try:
+                i = int(input, 2)
+                d = int(i)
+                print(d)
+            except:
+                print("The data you have input is not a binary integer")
+        elif num_system == "hexadecimal": 
+            try:
+                i = int(input, 16)
+                d = int(i)
+                print(d)
+            except:
+                print("The data you have input is not a hexadecimal integer")
+        elif num_system == "octal": 
+            try:
+                i = int(input, 8)
+                d = int(i)
+                print(d)
+            except:
+                print("The data you have input is not a octal integer")
     elif event == "Convert to Hexadecimal":
         if num_system == "decimal":
             try:
-                num = int(input)
-                h = hex(num)
+                i = int(input)
+                h = hex(i)
                 print(h)
             except:
                 print("The data you have input is not an integer")
+        elif num_system == "binary": 
+            try:
+                i = int(input, 2)
+                h = hex(i)
+                print(h)
+            except:
+                print("The data you have input is not a binary integer")
+        elif num_system == "hexadecimal":
+            print("You already have hexadecimal selected. You need to change to another number system in order to convert to hexadecimal.")
+        elif num_system == "octal": 
+            try:
+                i = int(input, 8)
+                h = hex(i)
+                print(h)
+            except:
+                print("The data you have input is not a octal integer")
     elif event == "Convert to Octal":
         if num_system == "decimal":
             try:
@@ -77,4 +142,20 @@ while continue_calculating:
                 print(o)
             except:
                 print("The data you have input is not an integer")
+        elif num_system == "binary": 
+            try:
+                i = int(input, 2)
+                o = oct(i)
+                print(o)
+            except:
+                print("The data you have input is not a binary integer")
+        elif num_system == "hexadecimal": 
+            try:
+                i = int(input, 16)
+                o = oct(i)
+                print(o)
+            except:
+                print("The data you have input is not a hexadecimal integer")
+        elif num_system == "octal":
+            print("You already have octal selected. You need to change to another number system in order to convert to octal.")        
     print("The current number system is " + num_system)
